1fe0fe0622f6bcaa4e2c406c5a2183b08365f485
Kennedy Lino Gyimah <linogyimahkennedy@gmail.com>
2023-11-21 08:25:57 +0000

    feat: Setup auth services

---
+++ lib/services/auth/auth_provider.dart
@@ -0,0 +1,18 @@
+
+import 'package:flutter_course_project/services/auth/auth_user.dart';
+
+abstract class AuthProvider {
+  Future<void> initialize();
+  AuthUser? get currentUser;
+  Future<AuthUser> logIn({
+    required String email,
+    required String password,
+  });
+  Future<AuthUser> createUser({
+    required String email,
+    required String password,
+  });
+  Future<void> logOut();
+  Future<void> sendEmailVerification();
+  Future<void> sendPasswordReset({required String toEmail});
+}
---
+++ lib/services/auth/auth_services.dart
@@ -0,0 +1,32 @@
+import 'package:flutter_course_project/services/auth/auth_provider.dart';
+import 'package:flutter_course_project/services/auth/auth_user.dart';
+
+class AuthServices implements AuthProvider {
+  final AuthProvider provider;
+
+  AuthServices({required this.provider});
+  @override
+  Future<AuthUser> createUser(
+          {required String email, required String password}) =>
+      provider.createUser(email: email, password: password);
+
+  @override
+  AuthUser? get currentUser => provider.currentUser;
+
+  @override
+  Future<void> initialize() => provider.initialize();
+
+  @override
+  Future<AuthUser> logIn({required String email, required String password}) =>
+      provider.logIn(email: email, password: password);
+
+  @override
+  Future<void> logOut() => provider.logOut();
+
+  @override
+  Future<void> sendEmailVerification() => provider.sendEmailVerification();
+
+  @override
+  Future<void> sendPasswordReset({required String toEmail}) =>
+      provider.sendPasswordReset(toEmail: toEmail);
+}
