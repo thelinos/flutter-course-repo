eefda794fa9d049e7bb77c9cf4a0b6d6ba9feb80
Kennedy Lino Gyimah <linogyimahkennedy@gmail.com>
2023-11-21 08:26:18 +0000

    feat: Setup views

---
+++ lib/views/notes/create_update_notes_views.dart
@@ -0,0 +1,12 @@
+
+
+import 'package:flutter/material.dart';
+
+class CreateUpdateNotesViews  extends StatelessWidget {
+  const CreateUpdateNotesViews ({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
